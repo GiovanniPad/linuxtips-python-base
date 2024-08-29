aad83ed1be22b6df512b0a5e3ad1871bd38b0b4b
Giovanni Padilha <geovannepadilha@hotmail.com>
2024-08-29 15:57:19 -0300

    Tornar o projeto executável closes #9

--- dundie-rewards/setup.py
+++ dundie-rewards/setup.py
@@ -12,5 +12,12 @@ setup(
     # Autor do programa
     author="Giovanni Padilha",
     # Busca todos os pacotes que tem `__init__.py` no diretório
-    packages=find_packages()
+    packages=find_packages(),
+    # Dicionário de entry points, onde a chave é o tipo do entry point e o valor são os caminhos para os entry points.
+    # Adicinando uma função `main()` do arquivo `__main__.py` como entry point de linha de console.
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie:__main__.main"
+        ]
+    }
 )
